ae8325dc30cc87e13fcc443814c13c69a90b7af2
Michael Vieck <vieck@purdue.edu>
2018-12-25 11:09:35 -0500

    Added skeleton weather page.

---
+++ lib/weather/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class WeatherPage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
