d3378c47415ab95e5d2e56d44af6df4b26a8f5fe
Michael Vieck <vieck@purdue.edu>
2019-04-19 23:06:25 -0400

    Fixed base url.

--- lib/data/repositories/WeatherRepository.dart
+++ lib/data/repositories/WeatherRepository.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/data/models/weather_response.dart';
 
 class WeatherRepository {
-  String baseUrl = "api.openweathermap.org/data/2.5/weather?q={city name}";
+  String baseUrl = "https://samples.openweathermap.org/data/2.5/";
 
   Future<WeatherResponse> getCurrentWeather() async {
     var client = new http.Client();
-    final response = await client.get(baseUrl);
+    final response = await client.get(baseUrl + "weather?q=Indianapolis");
     if (response.statusCode == 200) {
       return WeatherResponse.fromJson(json.decode(response.body));
     } else {
